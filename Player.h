3585731172cabeace4f28e952ac536d7a067c063
meromodel <43822405+meromodel@users.noreply.github.com>
2022-08-08 01:53:09 +0300

    copy assignment added

--- Player.h
+++ Player.h
@@ -12,14 +12,29 @@ class Player {
 
 
 public:
+
+// constructors
      ~Player();
     Player()= default;
     explicit Player(char * pName = nullptr, unsigned int pid = 0);
+
+// copy constructor
     Player(const Player & other);
-    friend std::ostream &operator<<(std::ostream & os, const Player& player);
+// copt assignment
+    Player &operator=(const Player &rhs);
+// getters
     char * get_name() const;
     unsigned int get_id() const;
     unsigned int get_score() const;
+
+// setters
+    void setName(char *name);
+
+    void setId(unsigned int id);
+
+    void setScore(unsigned int score);
+// class friends
+    friend std::ostream &operator<<(std::ostream &os, const Player &player);
 
 private:
     char * name{};
